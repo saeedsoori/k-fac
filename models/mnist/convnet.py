1afb89bdf1c883caa8b9a8fb36436f6c52c03adc
saeed soori <saeedsoori@saeeds-MacBook-Pro.local>
2021-03-11 10:32:43 -0500

    adding conv net

--- models/mnist/convnet.py
+++ models/mnist/convnet.py
@@ -8,18 +8,33 @@ __all__ = ['convnet']
 class ConvNet(nn.Module):
 
     def __init__(self, num_classes=10, **kwargs):
+        # super(ConvNet, self).__init__()
+        # self.features = nn.Sequential(
+        #     nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1),
+        #     nn.ReLU(),
+        #     nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
+        #     nn.ReLU(),
+        #     nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
+        #     nn.ReLU(),
+        #     nn.Flatten(), 
+        #     nn.Linear(28*28*32, 100),
+        #     nn.ReLU(),
+        #     nn.Linear(100, 100),
+        #     nn.ReLU(),
+        #     nn.Linear(100, 10)         
+        # )
         super(ConvNet, self).__init__()
         self.features = nn.Sequential(
-            nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(1, 8, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(8, 8, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(8, 8, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.Flatten(), 
-            nn.Linear(28*28*32, 100),
+            nn.Linear(28*28*8, 40),
             nn.ReLU(),
-            nn.Linear(100, 100),
+            nn.Linear(40, 100),
             nn.ReLU(),
             nn.Linear(100, 10)         
         )
