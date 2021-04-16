1f54c779447394955cfacb32d85b1918c529f2d0
saeed soori <saeedsoori@saeeds-MacBook-Pro.local>
2021-04-16 17:30:14 -0400

    fixing ekfac mnist script

--- mnist_fc_ekfac.sh
+++ mnist_fc_ekfac.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 epochs=10
 device=$@
-for lr in 1e-3 3e-3 1e-2 3e-2 1e-1 3e-1 1 3 10
+for lr in 1e-3 3e-3 1e-2 3e-2 1e-1 3e-1 1
 do
-	for damping in 3e-3 1e-2 3e-2 0.1 0.3 1 3
+	for damping in 3e-3 1e-2 3e-2 0.1 0.3
 	do
-   		python main.py --network fc --dataset mnist --learning_rate $lr --step_info true --momentum 0.9 --damping $damping --device $device --optimizer ekfac  --epoch $epochs   --weight_decay 0.003 --batch_size 128
+   		python main.py --network fc --dataset mnist --TScal 20 --learning_rate $lr --step_info true --momentum 0.9 --damping $damping --device $device --optimizer ekfac  --epoch $epochs   --weight_decay 0.003 --batch_size 128
 	done
 done
