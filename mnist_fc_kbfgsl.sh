3bf024675e39a265d6d579f96387ff9a6d3585f9
nobody <63745715+2zki3@users.noreply.github.com>
2021-04-16 15:38:15 -0400

    Fix device.

--- mnist_fc_kbfgsl.sh
+++ mnist_fc_kbfgsl.sh
@@ -1,7 +1,7 @@
 # python main.py --network fc --dataset mnist --learning_rate 0.1 --step_info true --momentum 0.9 --damping 0.3 --device cpu --optimizer kbfgsl  --epoch 5  --weight_decay 0.003 --batch_size 128
 #!/bin/bash
 epochs=10
-device=cpu
+device=$@
 for lr in 1e-3 3e-3 1e-2 3e-2 1e-1 3e-1 1 3 10
 do
 	for damping in 3e-3 1e-2 3e-2 0.1 0.3 1 3
