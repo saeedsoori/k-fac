85b11d08be2d37ce69b1cc3df939d9d194b83673
nobody <63745715+2zki3@users.noreply.github.com>
2021-04-16 21:47:17 -0400

    Add script for convnet + K-BFGS(L).

---
+++ scripts/mnist_convnet_kbfgsl.sh
@@ -0,0 +1,1 @@
+python main.py --network convnet --dataset mnist --device cpu --optimizer kbfgsl  --epoch 5 --learning_rate 0.1 --damping 0.3 --stat_decay 0.9 --weight_decay 0.003 --TCov 10 --TInv 50 --momentum 0.9
