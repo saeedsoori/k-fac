32eccccbe602b31b131564a6d605a457c94c5fbf
nobody <63745715+2zki3@users.noreply.github.com>
2021-04-16 22:26:36 -0400

    Use TCov = 20, TInv = 100 for K-BFGS and K-BFGS(L).

--- scripts/mnist_convnet_kbfgsl.sh
+++ scripts/mnist_convnet_kbfgsl.sh
@@ -1,1 +1,1 @@
-python main.py --network convnet --dataset mnist --device cpu --optimizer kbfgsl  --epoch 5 --learning_rate 0.1 --damping 0.3 --stat_decay 0.9 --weight_decay 0.003 --TCov 10 --TInv 50 --momentum 0.9
+python main.py --network convnet --dataset mnist --device cpu --optimizer kbfgsl  --epoch 5 --learning_rate 0.1 --damping 0.3 --stat_decay 0.9 --weight_decay 0.003 --TCov 20 --TInv 100 --momentum 0.9 --num_s_y_pairs 100
