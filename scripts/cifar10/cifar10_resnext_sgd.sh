a92714535ad93b7f7dcdfc5dbef1d4f262c8761b
saeed soori <saeedsoori@saeeds-MacBook-Pro.local>
2021-04-19 20:01:51 -0400

    fixing resnext scripts

--- scripts/cifar10/cifar10_resnext_sgd.sh
+++ scripts/cifar10/cifar10_resnext_sgd.sh
@@ -4,5 +4,5 @@ device=$@
 # range lr : 1e-3 1e-2 1e-1 opt:1e-2
 for lr in 1e-1 1e-2
 do
-	python main.py --dataset cifar10  --batch_size 128  --device $device --optimizer sgd --network resnext  --depth 19 --cardinality 12 --widen_factor 4 --epoch $epochs --milestone 10,20,30,40,50 --learning_rate  $lr --learning_rate_decay 0.5  --weight_decay 0.003 --momentum 0.9
+	python main.py --dataset cifar10  --batch_size 128  --device $device --optimizer sgd --network resnext  --depth 19 --cardinality 1 --widen_factor 4 --base_width 64 --epoch $epochs --milestone 10,20,30,40,50 --learning_rate  $lr --learning_rate_decay 0.5  --weight_decay 0.003 --momentum 0.9
 done
