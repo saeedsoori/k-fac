844bcce08c8cca39e2546e864124b5dc8e78b71a
nobody <63745715+2zki3@users.noreply.github.com>
2021-04-28 07:45:21 -0400

    Add parameter tuning scripts for each optimizer.

---
+++ scripts/fashion-mnist/convnet/fashion-mnist_convnet_kbfgsl.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+epochs=10
+device=$@
+
+for lr in 1e-3 3e-3 1e-2 3e-2 1e-1 3e-1
+do
+	for damping in 1e-3 3e-3 1e-2 3e-2 1e-1 3e-1
+	do
+		for wd in 1e-3 3e-3 1e-2 3e-2
+		do
+   			python main.py --network convnet --dataset fashion-mnist --learning_rate $lr --step_info true --momentum 0.9 --damping $damping --device $device --optimizer kbfgsl --epoch $epochs --milestone 10,20,30,40,50 --weight_decay $wd --batch_size 128
+		done
+	done
+done
