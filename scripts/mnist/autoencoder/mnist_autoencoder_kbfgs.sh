91bf06ec1f57ba140e2430e491c319b3cbd717c0
nobody <63745715+2zki3@users.noreply.github.com>
2021-06-01 00:31:07 -0400

    Add autoencoder scripts.

---
+++ scripts/mnist/autoencoder/mnist_autoencoder_kbfgs.sh
@@ -0,0 +1,2 @@
+device=$@
+python main_autoencoder.py --network autoencoder --dataset mnist --device $device --optimizer kbfgs  --epoch 5 --learning_rate 0.3 --damping 0.3 --stat_decay 0.9 --weight_decay 0 --TCov 1 --TInv 1 --momentum 0.9 --batch_size 1000
