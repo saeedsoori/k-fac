7590ca04918bf4a4719f497bc264609f17146157
nobody <63745715+2zki3@users.noreply.github.com>
2021-06-02 23:43:32 -0400

    Add toy model to plot Fisher inv.

---
+++ models/mnist/toy.py
@@ -0,0 +1,32 @@
+import torch.nn as nn
+
+
+__all__ = ['toy']
+
+
+class ToyNet(nn.Module):
+
+    def __init__(self, num_classes=10, **kwargs):
+        
+        super(ToyNet, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3),
+            nn.Conv2d(16, 16, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3),
+            nn.Conv2d(16, 16, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Flatten(), 
+            nn.Linear(3*3*16, 10)
+        )
+
+    def forward(self, x, bfgs=False):
+        x = self.features(x)
+        return x
+
+
+def toy(**kwargs):
+    model = ToyNet(**kwargs)
+    return model
