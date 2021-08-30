8dcf8b7f8853a632202109ed7fb1e6c51259d452
Ubuntu <ubuntu@ip-172-31-21-87.ec2.internal>
2021-08-30 18:05:19 +0000

    integrating batch matmul with TENGraD

---
+++ benchmark_bmm.py
@@ -0,0 +1,189 @@
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import math
+import time
+
+import torch
+from torch.profiler import profile, record_function, ProfilerActivity
+
+
+TIME_SCALES = {'s': 1, 'ms': 1000, 'us': 1000000}
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-b', '--batch-size', type=int, default=128)
+parser.add_argument('-r', '--runs', type=int, default=100)
+parser.add_argument('--scale', choices=['s', 'ms', 'us'], default='us')
+parser.add_argument('-n', '--n', type=int, default=20)
+parser.add_argument('-m', '--mode', type=str, default='all')
+parser.add_argument('-p', '--pytorch', type=str, default='true')
+parser.add_argument('-v', '--debug', type=str, default='false')
+parser.add_argument('-l','--colm', nargs='+', default= [32, 64, 128, 256, 512])
+options = parser.parse_args()
+
+# change this line
+from utils.bmm import BMM
+
+
+
+options.cuda = True
+device = torch.device("cuda") if options.cuda else torch.device("cpu")
+dtype = torch.float32
+
+kwargs = {'dtype': dtype,
+          'device': device,
+          'requires_grad': False}
+
+# generate "n" random matrix with different #columns
+r_size = [int(i) for i in options.colm]
+# r_size = [100 400]
+# r_size = [8]
+A = []
+B = []
+C = []
+
+mshapes = []
+nshapes = []
+kshapes = []
+index = torch.randint(0, len(r_size), (options.n,))
+
+
+sum_size_A = 0
+sum_size_B = 0
+sum_size_C = 0
+C_true=[]
+for i in range(options.n):
+    A_s = torch.randn(options.batch_size, r_size[index[i]], **kwargs)
+    B_s = torch.randn(r_size[index[i]], r_size[index[i]] , **kwargs)
+    C_s = torch.zeros(options.batch_size, r_size[index[i]] , **kwargs)
+
+    C_true.append(torch.matmul(A_s, B_s))
+
+    A.append(A_s)
+    B.append(B_s)
+    C.append(C_s)
+    
+    mshapes.append(A_s.shape[0])
+    nshapes.append(B_s.shape[1])
+    kshapes.append(A_s.shape[1])
+
+    sum_size_A = sum_size_A + A_s.numel()
+    sum_size_B = sum_size_B + B_s.numel()
+    sum_size_C = sum_size_C + C_s.numel()
+
+# adding one extra elements since magma needs it
+mshapes.append(0)
+nshapes.append(0)
+kshapes.append(0)
+
+### making a contiguous tensor
+A_con = torch.zeros(sum_size_A, **kwargs)
+B_con = torch.zeros(sum_size_B, **kwargs)
+C_con = torch.zeros(sum_size_C, **kwargs)
+# C_con_magma = torch.zeros(sum_size_C, **kwargs)
+
+# print('tensors created with size:', sum_size_A)
+offset_A = 0
+offset_B = 0
+offset_C = 0
+all_offset_A = [offset_A]
+all_offset_B= [offset_B]
+all_offset_C = [offset_C]
+for i in range(options.n):
+    A_con[0 + offset_A:A[i].numel() + offset_A] = torch.reshape(A[i], [1,-1])
+    B_con[0 + offset_B:B[i].numel() + offset_B] = torch.reshape(B[i], [1,-1])
+    C_con[0 + offset_C:C[i].numel() + offset_C] = torch.reshape(C[i], [1,-1])
+    # C_con_magma[0 + offset_C:C[i].numel() + offset_C] = torch.reshape(C[i], [1,-1])
+    offset_A = offset_A + A[i].numel()
+    offset_B = offset_B + B[i].numel()
+    offset_C = offset_C + C[i].numel()
+    all_offset_A.append(offset_A)
+    all_offset_B.append(offset_B)
+    all_offset_C.append(offset_C)
+
+
+
+# m_arr = torch.cuda.IntTensor(mshapes).to('cpu')
+# n_arr = torch.cuda.IntTensor(nshapes).to('cpu')
+# k_arr = torch.cuda.IntTensor(kshapes).to('cpu')
+
+
+m_magma = torch.cuda.IntTensor(mshapes)
+n_magma = torch.cuda.IntTensor(nshapes)
+k_magma = torch.cuda.IntTensor(kshapes)
+
+m_arr = mshapes
+n_arr = nshapes
+k_arr = kshapes
+
+Mul = BMM(A_con, B_con, C_con, options.n, all_offset_A, all_offset_B, all_offset_C, m_magma, n_magma, k_magma)
+# C_con = torch.zeros(sum_size_C, **kwargs)
+
+# C_s_true_all=[]
+
+pytorch_time = 0
+
+if options.pytorch == 'true':
+    for j in range(options.runs):
+        for i in range(options.n):
+            torch.cuda.synchronize()
+            start = time.time()
+            C_s_true = torch.matmul(A[i], B[i])
+            torch.cuda.synchronize()
+            elapsed = time.time() - start
+            pytorch_time += elapsed
+        # C_s_true_all.append(C_s_true)
+    
+torch.cuda.synchronize()
+
+
+cublas_time = 0
+if options.mode == 'cublas' or options.mode == 'all':
+    print('performing cublas operations...')
+    for j in range(options.runs):
+        torch.cuda.synchronize()
+        start = time.time()
+        result = Mul.CublasForward(A_con, B_con, C_con, m_arr, n_arr, k_arr, options.n, all_offset_A, all_offset_B, all_offset_C)
+        torch.cuda.synchronize()
+        elapsed = time.time() - start
+        cublas_time += elapsed
+#   
+magma_time = 0
+if options.mode == 'magma' or options.mode == 'all':
+    print('performing magma operations...')
+    for j in range(options.runs):
+        torch.cuda.synchronize()
+        start = time.time()
+        result = Mul.MagmaForward(A_con, B_con, C_con, m_magma, n_magma, k_magma, options.n)
+        torch.cuda.synchronize()
+        elapsed = time.time() - start
+        magma_time += elapsed
+
+
+
+print('checking that the error is near zero')
+# if options.mode == 'all':
+for k in range(options.n):
+    C_ = C_con[0 + all_offset_C[k]: C_true[k].numel() + all_offset_C[k]]
+    C_ = C_.view_as(C_true[k])
+    if not torch.allclose(C_, C_true[k]) and options.debug == 'true':
+        print('A:', A[k])
+        print('B:', B[k])
+        print("C True: L2 and Fro norms are: %s and %s" % (torch.linalg.norm(C_, ord=2), torch.linalg.norm(C_, ord='fro')))
+        print('C Comp: L2 and Fro norms are: %s and %s' % (torch.linalg.norm(C_true[k], ord=2), torch.linalg.norm(C_true[k], ord='fro')))
+        print('matmul error w.r.t pytorch:', C_-C_true[k])
+print('#'*20)
+        
+
+ 
+scale = TIME_SCALES[options.scale]
+pytorch_average = pytorch_time / options.runs * scale
+cublas_average = cublas_time / options.runs * scale
+magma_average = magma_time / options.runs * scale
+
+print('PyTorch: {0:.3f} {3} | cublas {1:.3f} {3} | magma {2:.3f} {3}'.format(
+     pytorch_average,  cublas_average, magma_average,
+    options.scale))
+
+
---
+++ utils/bmm_cuda.cpp
@@ -0,0 +1,223 @@
+#include <torch/extension.h>
+
+#include <vector>
+#include <pybind11/pybind11.h>
+
+
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <cublas_v2.h>
+#include <omp.h>
+#include "magma_v2.h"
+#include "magma_lapack.h"
+
+// CUDA forward declarations
+namespace py = pybind11;
+
+
+// Pulled from magma test code
+#define TESTING_CHECK( err )                                                 \
+    do {                                                                     \
+        magma_int_t err_ = (err);                                            \
+        if ( err_ != 0 ) {                                                   \
+            fprintf( stderr, "Error: %s\nfailed at %s:%d: error %lld: %s\n", \
+                     #err, __FILE__, __LINE__,                               \
+                     (long long) err_, magma_strerror(err_) );               \
+            exit(1);                                                         \
+        }                                                                    \
+    } while( 0 )
+
+// C++ interface
+
+// NOTE: AT_ASSERT has become AT_CHECK on master after 0.4.
+#define CHECK_CUDA(x) AT_ASSERTM(x.type().is_cuda(), #x " must be a CUDA tensor")
+#define CHECK_CONTIGUOUS(x) AT_ASSERTM(x.is_contiguous(), #x " must be contiguous")
+#define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
+
+
+
+class BatchMatmul{
+
+public:
+
+	cudaStream_t *streams;
+    cublasHandle_t *handles;
+
+    magma_queue_t queue;
+  	magma_device_t device;
+
+    int* magma_m;
+    int* magma_n;
+    int* magma_k;
+
+	float ** A_array;
+  	float ** B_array;
+  	float ** C_array;
+
+  	float const* * dA_array;
+  	float const* * dB_array;
+  	float **dC_array;
+
+	void set_pointers(
+		torch::Tensor A,
+    	torch::Tensor B,
+    	torch::Tensor C,
+    	int batchCount,
+    	std::vector<int> offset_A,
+    	std::vector<int> offset_B,
+    	std::vector<int> offset_C){
+
+  	magma_queue_t queue;
+  	magma_device_t device;
+  	// std::cout<<"initialization finsihed..."<<"\n";
+
+  	magma_getdevice( &device );
+  	magma_queue_create( device, &queue );
+
+  	TESTING_CHECK( magma_malloc_cpu( (void**)&A_array, sizeof(float*)*batchCount ) );
+  	TESTING_CHECK( magma_malloc_cpu( (void**)&B_array, sizeof(float*)*batchCount ) );
+  	TESTING_CHECK( magma_malloc_cpu( (void**)&C_array, sizeof(float*)*batchCount ) );
+
+
+  	for (int i = 0; i < batchCount; ++i)
+  	{
+    // std::cout<<"processing input tensor:"<< i<< " \n";
+
+    	A_array[i] = (float *) A.data_ptr() + offset_A[i];
+    	B_array[i] = (float *) B.data_ptr() + offset_B[i];
+    	C_array[i] = (float *) C.data_ptr() + offset_C[i];
+  	}
+
+
+
+
+    TESTING_CHECK( magma_malloc( (void**)&dA_array, sizeof(float*)*batchCount ) );
+  	TESTING_CHECK( magma_malloc( (void**)&dB_array, sizeof(float*)*batchCount ) );
+  	TESTING_CHECK( magma_malloc( (void**)&dC_array, sizeof(float*)*batchCount ) );
+
+  	magma_setvector(batchCount, sizeof(float*), A_array, 1, dA_array, 1, queue);
+  	magma_setvector(batchCount, sizeof(float*), B_array, 1, dB_array, 1, queue);
+  	magma_setvector(batchCount, sizeof(float*), C_array, 1, dC_array, 1, queue);
+
+  	
+
+  };
+
+  void magma_initialize(
+  	torch::Tensor m_,
+    torch::Tensor n_,
+    torch::Tensor k_){
+
+  	magma_getdevice( &device );
+  	magma_queue_create( device, &queue );
+
+  	magma_m  = (int*) m_.data_ptr();
+  	magma_n = (int*) n_.data_ptr();
+  	magma_k = (int*) k_.data_ptr();
+        
+    };
+
+
+  void make_streams(int batchCount){
+   streams = (cudaStream_t *) malloc(batchCount*sizeof(cudaStream_t));
+   handles = (cublasHandle_t *) malloc(batchCount*sizeof(cublasHandle_t));
+   for(int i=0; i<batchCount; i++){
+        cudaStreamCreate(&streams[i]);
+
+   //cudaStreamCreate(&stream);
+   cublasCreate(&handles[i]);
+  //cublasSetStream(handles[i],streams[i]);
+  }
+  };
+
+  void launch_kernel(int n, int m, int k, float* B, float* A, float* C, cudaStream_t stream, cublasHandle_t handle)
+{
+
+  cublasSetStream(handle,stream);
+  float alpha=1.0f;
+  float beta=0.0f;
+  cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, n, m, k, &alpha, B, n, A, k, &beta, C, n);
+
+};
+
+
+
+
+  int CublasForward(
+    torch::Tensor A,
+    torch::Tensor B,
+    torch::Tensor C,
+    std::vector<int> m, std::vector<int> n, std::vector<int> k, int batchCount,
+    std::vector<int> offset_A,
+    std::vector<int> offset_B,
+    std::vector<int> offset_C
+    ) {
+
+
+#pragma omp parallel
+#pragma omp for
+for(int i=0; i<batchCount; i++){
+
+   launch_kernel(n[i], m[i], k[i],  (float *) (B.data_ptr() )+ offset_B[i], (float *) (A.data_ptr() )+ offset_A[i], (float *) (C.data_ptr() )+ offset_C[i], streams[i], handles[i]);
+
+}
+};
+
+
+
+int MagmaForward(
+    torch::Tensor m_,
+    torch::Tensor n_,
+    torch::Tensor k_,
+    int batch_size) {
+  
+  float  alpha = 1.0;
+  float  beta = 0.0;
+
+
+  magma_trans_t transA = MagmaNoTrans;
+  magma_trans_t transB = MagmaNoTrans;
+
+  magma_int_t batchCount = batch_size;
+  
+
+  magmablas_sgemm_vbatched(transA,transB, magma_n,
+      /* magma_int_t * */         magma_m,
+      /* magma_int_t * */         magma_k,
+      /* double */                alpha,
+      /* double const *const * */ dB_array,
+      /* magma_int_t */           magma_n,
+      /* double const *const * */ dA_array,
+      /* magma_int_t * */         magma_k,
+      /* double */                beta,
+      /* double ** */             dC_array,
+      /* magma_int_t * */         magma_n,
+      /* magma_int_t */           batchCount,
+      /* magma_queue_t */         queue);
+
+  return 2;
+}
+
+
+};
+
+
+
+
+
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+	std::string name = std::string("BatchMatmul");
+
+	py::class_<BatchMatmul>(m, name.c_str())
+      .def(py::init<>())
+      // .def("setKey", &Foo::setKey)
+      .def("set_pointers", &BatchMatmul::set_pointers)
+      .def("CublasForward", &BatchMatmul::CublasForward)
+      .def("make_streams", &BatchMatmul::make_streams)
+      .def("magma_initialize", &BatchMatmul::magma_initialize)
+      .def("MagmaForward", &BatchMatmul::MagmaForward);
+      // .def("getKey", &Foo::getKey);
+  // m.def("forward", &bmm_forward, "BMM forward (CUDA)");
+  // m.def("cublas_gemm_call", &cublas_forward, "BMM cublas_gemm_call (CUDA)");
+}
+
